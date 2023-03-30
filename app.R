f134850f8f044bf48943626563d9a3172923b4b9
Nathan DeVoss <Nathan2@cdib-Randi-Wood-MacBook-Air.local>
2023-03-30 15:29:12 -0500

    removed vestigial R/

--- app.R
+++ app.R
@@ -11,7 +11,6 @@ options(shiny.maxRequestSize = 7*10^9)
 
 source("ui.R")
 source("server.R")
-miceadds::source.all("R/cosia_scripts", grepstring = ".R", print.source = FALSE)
 
 #usethis::use_pipe(export = TRUE)
 shinyApp(ui = ui, server = server)
